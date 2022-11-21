deaacd70a09614464e14d2d60ff0b923ceb1ff16
Mhmdh <mohammad.hammoud.lb@hotmail.com>
2022-11-22 00:25:46 +0300

    Added new props

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { useState, useEffect } from 'react';
 import STLViewer from './STLViewer';
 import Slider from 'react-input-slider';
 const App = (props) => {
-  const { models, color, rotate = false } = props;
+  // const {
+  //   rotate = false,
+  //   range = false,
+  //   remove = false,
+  //   handleRemove = () => {},
+  // } = props;
+  const {
+    models,
+    color,
+    rotate = false,
+    range = false,
+    remove = false,
+    handleRemove = () => {},
+  } = props;
   // const [color, setColor] = useState('rgba(255,0,48,0.2)');
   // const [models, setModels] = useState([
   //   // 'https://hassans.s3.eu-central-1.amazonaws.com/youssef/order/order_g69r/impressions/order_g69rL.STL',
-  //   'https://hassans.s3.eu-central-1.amazonaws.com/assets/002r_outputmodel09-04-2022:20:09:47.stl',
-  //   'https://hassans.s3.eu-central-1.amazonaws.com/assets/002r_faceplatemodel09-04-2022:20:09:2.stl',
-  //   'https://hassans.s3.eu-central-1.amazonaws.com/assets/002r_inputmodel09-05-2022:14:09:33.STL',
+  //   'https://hassans.s3.eu-central-1.amazonaws.com/youssef/assets/w4fxL_OutputModel_SLA.stl',
   // ]);
   const [sliders, setSliders] = useState([]);
   useEffect(() => {
@@ -21,52 +32,70 @@ const App = (props) => {
   }, [models]);
   if (!models || sliders.length === 0) return null;
   return (
-    <div>
+    <div style={{ width: '400px' }}>
       <div style={{ margin: '10px 0px' }}>
-        {sliders.map((item, index) => {
-          return (
-            <div
-              key={index}
-              style={{
-                margin: '10px 0px',
-              }}
-            >
-              <input
-                type="checkbox"
-                onClick={(e) => {
-                  const newSliders = sliders.map((item, i) => {
-                    if (i === index) {
-                      if (e.target.checked) {
-                        return { ...item, value: 0 };
-                      } else {
-                        return { ...item, value: 1 };
-                      }
-                    }
-                    return item;
-                  });
-                  setSliders(newSliders);
+        {range &&
+          sliders.map((item, index) => {
+            return (
+              <div
+                key={index}
+                style={{
+                  margin: '10px 0px',
+                  display: 'flex',
+                  alignItems: 'center',
+                  justifyContent: 'space-between',
                 }}
-              />
-              <Slider
-                axis="x"
-                xstep={0.1}
-                xmin={0.0}
-                xmax={1.0}
-                x={item.value}
-                onChange={({ x }) => {
-                  const newSliders = sliders.map((item, i) => {
-                    if (i === index) {
-                      return { ...item, value: parseFloat(x) };
-                    }
-                    return item;
-                  });
-                  setSliders(newSliders);
-                }}
-                style={{ marginLeft: '20px' }}
-              />
-            </div>
-          );
-        })}
+              >
+                <div>
+                  <input
+                    type="checkbox"
+                    checked={sliders[index].value > 0}
+                    onClick={(e) => {
+                      const newSliders = sliders.map((item, i) => {
+                        if (i === index) {
+                          if (e.target.checked) {
+                            return { ...item, value: 1 };
+                          } else {
+                            return { ...item, value: 0 };
+                          }
+                        }
+                        return item;
+                      });
+                      setSliders(newSliders);
+                    }}
+                  />
+                  <Slider
+                    axis="x"
+                    xstep={0.1}
+                    xmin={0.0}
+                    xmax={1.0}
+                    x={item.value}
+                    onChange={({ x }) => {
+                      const newSliders = sliders.map((item, i) => {
+                        if (i === index) {
+                          return { ...item, value: parseFloat(x) };
+                        }
+                        return item;
+                      });
+                      setSliders(newSliders);
+                    }}
+                    style={{ marginLeft: '20px' }}
+                  />
+                </div>
+                {remove && (
+                  <a role="button">
+                    <img
+                      src="https://hassans.s3.eu-central-1.amazonaws.com/assets/x11-21-2022:21:11:19.png"
+                      style={{ width: '21px', cursor: 'pointer' }}
+                      onClick={() => {
+                        handleRemove(models[index]);
+                      }}
+                    />
+                  </a>
+                )}
+              </div>
+            );
+          })}
       </div>
       <STLViewer
         modelColor={color}
